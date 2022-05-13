ed0d4062029231ecd8eef967dc08dae732487a8e
Rumon360 <hmk.rumon@gmail.com>
2022-05-13 23:32:59 +0600

    Updates

---
+++ precache-manifest.73efbefad558ca75a3b9e14efbb79d12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ddfbe4e25c5698cf04d8df0640e941c",
+    "url": "/PokemonThreeJs/index.html"
+  },
+  {
+    "revision": "cd1cdd329023b21eb926",
+    "url": "/PokemonThreeJs/static/css/main.a2081d7f.chunk.css"
+  },
+  {
+    "revision": "bd71c1d377ac67627669",
+    "url": "/PokemonThreeJs/static/js/2.7e8ad5f6.chunk.js"
+  },
+  {
+    "revision": "c9b31f27ce0803bcbda04bfd0eba249e",
+    "url": "/PokemonThreeJs/static/js/2.7e8ad5f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd1cdd329023b21eb926",
+    "url": "/PokemonThreeJs/static/js/main.2368b9f2.chunk.js"
+  },
+  {
+    "revision": "2f3ad6b5b6dc1fa0e158",
+    "url": "/PokemonThreeJs/static/js/runtime-main.d50ceca4.js"
+  }
+]);
